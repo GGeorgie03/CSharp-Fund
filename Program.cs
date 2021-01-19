ee188932bf04486456bb04442409806e050aeeb9
GGeorgie03 <71280114+GGeorgie03@users.noreply.github.com>
2021-01-19 15:36:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01.__SoftUni_Reception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int allStudents = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int hourCounter = 0;
+            int all = 0;
+
+            while (all <= allStudents)
+            {
+                if (all == allStudents)
+                {
+                    break;
+                }
+                hourCounter++;
+                if (hourCounter % 4 == 0)
+                {
+                    
+                    continue;
+                }
+
+                sum = first + second + third;
+                all += sum;
+            }
+
+            Console.WriteLine($"Time needed: {hourCounter}h.");
+        }
+    }
+}
